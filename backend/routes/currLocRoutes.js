a1d451e5e2e63d6a06d02a11f34bb826fe2ae42a
saikumar1007 <165930951+saikumar1007@users.noreply.github.com>
2025-08-27 19:31:13 +0530

    Delete backend directory

--- backend/routes/currLocRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getCurrLocation } from "../controllers/currLocationController.js";
-
-const router = express.Router();
-
-router.get("/", getCurrLocation);
-
-export default router;
--- backend/routes/serviceRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getNearbyServices } from "../controllers/serviceController.js";
-
-const router = express.Router();
-
-router.get("/nearby", getNearbyServices);
-
-export default router;
